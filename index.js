baba1c49f312052739e33ebff71f0fa4f8d61cb8
Eyejangg <ptwptw1550@gmail.com>
2024-03-01 11:13:14 +0700

    12

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const mysql = require("mysql2");
 
 const app = express();
+app.use(express.json());
 
 app.use(cors());
 
@@ -25,6 +26,22 @@ app.get("/foodmenu", (req, res) => {
   connection.query("SELECT foodID, foodName, foodDescription, foodPrice, menuName FROM tbl_food , tbl_menu WHERE tbl_food.MenuID = tbl_menu.menuID", function (err, results, fields) {
     res.send(results);
   });
+});
+
+app.post("/addmenu", function (req, res)  {
+  connection.query(
+    "INSERT INTO `tbl_menu`(`menuID`, `menuName`) VALUES (?, ?)",
+    [req.body.menuID, req.body.menuName],
+
+    function (err, results) {
+      if (err) throw err;
+      return res.send({
+        err: false,
+        data: results,
+        message: "New menu has been created successfully.",
+      });
+    }
+  );
 });
 
 app.listen(process.env.PORT || 3000);
